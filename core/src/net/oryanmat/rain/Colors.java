7e89915371447c4a06b9bf70461083446cc9957c
oryanm <oryan.mat@gmail.com>
2015-06-06 15:25:24 +0300

    dumb

--- core/src/net/oryanmat/rain/Colors.java
+++ core/src/net/oryanmat/rain/Colors.java
@@ -11,7 +11,7 @@ import java.util.List;
  * Colors are taken from <a href="https://www.google.com/design/spec/style/color.html">Android's Material Design guide</a>
  */
 public class Colors {
-    enum PrimeryColors {
+    enum PrimaryColors {
         BLUE,
         LIGHT_BLUE,
         CYAN,
@@ -30,48 +30,35 @@ public class Colors {
         INDIGO
     }
 
-    static final ObjectMap<PrimeryColors, List<Color>> COLORS = new ObjectMap<PrimeryColors, List<Color>>() {{
-        put(PrimeryColors.BLUE, Arrays.asList(Blues.A200, Blues.A400, Blues.A700));
-        put(PrimeryColors.LIGHT_BLUE, Arrays.asList(LightBlues.A200, LightBlues.A400, LightBlues.A700));
-        put(PrimeryColors.CYAN, Arrays.asList(Cyans.A200, Cyans.A400, Cyans.A700));
-        put(PrimeryColors.TEAL, Arrays.asList(Teals.A200, Teals.A400, Teals.A700));
-        put(PrimeryColors.GREEN, Arrays.asList(Greens.A200, Greens.A400, Greens.A700));
-        put(PrimeryColors.LIGHT_GREEN, Arrays.asList(LightGreens.A200, LightGreens.A400, LightGreens.A700));
-        put(PrimeryColors.LIME, Arrays.asList(Limes.A200, Limes.A400, Limes.A700));
-        put(PrimeryColors.YELLOW, Arrays.asList(Yellows.A200, Yellows.A400, Yellows.A700));
-        put(PrimeryColors.AMBER, Arrays.asList(Ambers.A200, Ambers.A400, Ambers.A700));
-        put(PrimeryColors.ORANGE, Arrays.asList(Oranges.A200, Oranges.A400, Oranges.A700));
-        put(PrimeryColors.DEEP_ORANGE, Arrays.asList(DeepOranges.A200, DeepOranges.A400, DeepOranges.A700));
-        put(PrimeryColors.RED, Arrays.asList(Reds.A200, Reds.A400, Reds.A700));
-        put(PrimeryColors.PINK, Arrays.asList(Pinks.A200, Pinks.A400, Pinks.A700));
-        put(PrimeryColors.PURPLE, Arrays.asList(Purples.A200, Purples.A400, Purples.A700));
-        put(PrimeryColors.DEEP_PURPLE, Arrays.asList(DeepPurples.A200, DeepPurples.A400, DeepPurples.A700));
-        put(PrimeryColors.INDIGO, Arrays.asList(Indigos.A200, Indigos.A400, Indigos.A700));
+    static final ObjectMap<PrimaryColors, List<Color>> COLORS = new ObjectMap<PrimaryColors, List<Color>>() {{
+        put(PrimaryColors.BLUE, Arrays.asList(Blues.A200, Blues.A400, Blues.A700));
+        put(PrimaryColors.LIGHT_BLUE, Arrays.asList(LightBlues.A200, LightBlues.A400, LightBlues.A700));
+        put(PrimaryColors.CYAN, Arrays.asList(Cyans.A200, Cyans.A400, Cyans.A700));
+        put(PrimaryColors.TEAL, Arrays.asList(Teals.A200, Teals.A400, Teals.A700));
+        put(PrimaryColors.GREEN, Arrays.asList(Greens.A200, Greens.A400, Greens.A700));
+        put(PrimaryColors.LIGHT_GREEN, Arrays.asList(LightGreens.A200, LightGreens.A400, LightGreens.A700));
+        put(PrimaryColors.LIME, Arrays.asList(Limes.A200, Limes.A400, Limes.A700));
+        put(PrimaryColors.YELLOW, Arrays.asList(Yellows.A200, Yellows.A400, Yellows.A700));
+        put(PrimaryColors.AMBER, Arrays.asList(Ambers.A200, Ambers.A400, Ambers.A700));
+        put(PrimaryColors.ORANGE, Arrays.asList(Oranges.A200, Oranges.A400, Oranges.A700));
+        put(PrimaryColors.DEEP_ORANGE, Arrays.asList(DeepOranges.A200, DeepOranges.A400, DeepOranges.A700));
+        put(PrimaryColors.RED, Arrays.asList(Reds.A200, Reds.A400, Reds.A700));
+        put(PrimaryColors.PINK, Arrays.asList(Pinks.A200, Pinks.A400, Pinks.A700));
+        put(PrimaryColors.PURPLE, Arrays.asList(Purples.A200, Purples.A400, Purples.A700));
+        put(PrimaryColors.DEEP_PURPLE, Arrays.asList(DeepPurples.A200, DeepPurples.A400, DeepPurples.A700));
+        put(PrimaryColors.INDIGO, Arrays.asList(Indigos.A200, Indigos.A400, Indigos.A700));
     }};
 
+    /**
+     * the primary color is selected according to the level
+     * secondary shade is randomly selected out of 3
+     */
     public static Color getColor(int level) {
+        PrimaryColors primary = PrimaryColors.values()[((level - 1) % (PrimaryColors.values().length / 2))];
         int random = MathUtils.random(0, 2);
-        switch (((level - 1) % 16) + 1) {
-            default:
-            case 1: return COLORS.get(PrimeryColors.BLUE).get(random);
-            case 2: return COLORS.get(PrimeryColors.LIGHT_BLUE).get(random);
-            case 3: return COLORS.get(PrimeryColors.CYAN).get(random);
-            case 4: return COLORS.get(PrimeryColors.TEAL).get(random);
-            case 5: return COLORS.get(PrimeryColors.GREEN).get(random);
-            case 6: return COLORS.get(PrimeryColors.LIGHT_GREEN).get(random);
-            case 7: return COLORS.get(PrimeryColors.LIME).get(random);
-            case 8: return COLORS.get(PrimeryColors.YELLOW).get(random);
-            case 9: return COLORS.get(PrimeryColors.AMBER).get(random);
-            case 10: return COLORS.get(PrimeryColors.ORANGE).get(random);
-            case 11: return COLORS.get(PrimeryColors.DEEP_ORANGE).get(random);
-            case 12: return COLORS.get(PrimeryColors.RED).get(random);
-            case 13: return COLORS.get(PrimeryColors.PINK).get(random);
-            case 14: return COLORS.get(PrimeryColors.PURPLE).get(random);
-            case 15: return COLORS.get(PrimeryColors.DEEP_PURPLE).get(random);
-            case 16: return COLORS.get(PrimeryColors.INDIGO).get(random);
-        }
-    }
 
+        return COLORS.get(primary).get(random);
+    }
 
     static class Blues {
         public static final Color A200 = new Color(0x448AFFFF);
